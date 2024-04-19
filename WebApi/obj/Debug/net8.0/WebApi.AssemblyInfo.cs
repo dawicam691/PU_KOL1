5fc2e7e5eac6a8f6432d40bf56e01b121fff6a39
Student RM4-3073 04 <studentlab1404@it.polsl.pl>
2024-04-19 19:08:20 +0200

    Dziewiaty

--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f504a63127dd0bd1265444d4fad0fa03a7c5ce3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9952039e5e654349161fb28b88f5b7d80939d7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
