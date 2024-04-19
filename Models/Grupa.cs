a9b2dc7d216c83cfd28d602247337f799052f8ee
Student RM4-3073 04 <studentlab1404@it.polsl.pl>
2024-04-19 16:04:25 +0200

    drugi

---
+++ Models/Grupa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Grupa
+    {
+        public int Id { get; set; }
+        public string Nazwa { get; set; }
+        public List<Student> Studenci { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public int? IdGrupy { get; set; }
+        [ForeignKey(nameof(IdGrupy))]
+        public Grupa Grupa { get; set; }
+    }
+}
